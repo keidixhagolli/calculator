689d9a34bc16a78ac02bfc955cefa375fac7b839
keidixhagolli <40328926+keidixhagolli@users.noreply.github.com>
2018-06-17 18:11:36 -0400

    Read command line arguments.

---
+++ Calc.py
@@ -0,0 +1,4 @@
+firstnumber = input("Enter first number: ")
+action = input("Enter desired operation: ")
+secondnumber = input("Enter second number: ")
+print(firstnumber,action,secondnumber,"=",(firstnumber)(action)(secondnumber))
